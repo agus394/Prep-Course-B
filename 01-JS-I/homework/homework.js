4ec89a62adbc189dfc1a96c1188b1b54a7cd237a
Agustin Muñoz <agustinmunoz1811@gmail.com>
2022-11-22 14:29:28 -0300

    hm terminado

--- 01-JS-I/homework/homework.js
+++ 01-JS-I/homework/homework.js
@@ -2,7 +2,7 @@
 
 // Crea una variable "string", puede contener lo que quieras:
 const nuevaString = null;
-dfbdfbknagustin394
+dfbdfbknagustin39373
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = null;
 dvadfasdasa
